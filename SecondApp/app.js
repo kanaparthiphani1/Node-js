80bebaecb2cf920331cb6aebbc7321e2a86d7688
kanaparthiphani1 <kanaparthiphani0@gmail.com>
2023-02-04 10:27:45 +0530

    feat: Added routing and worked with req and res

--- SecondApp/app.js
+++ SecondApp/app.js
@@ -1,7 +1,38 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>First Node App</title></head>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' name='message' /> <button type='submit'>Send</button></form></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  } else if (url === "/message" && method === "POST") {
+    const reqBody = [];
+    req.on("data", (chunk) => {
+      reqBody.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(reqBody).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+    });
+
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>First Node App</title></head>");
+  res.write("<body><h1>Hello World!<h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(3000);
